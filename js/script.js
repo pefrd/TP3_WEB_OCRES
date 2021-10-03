1eda6108a795ce519c1db2aa798daf3c013f5293
pefrd <pierre-erwan.fouillard@edu.ece.fr>
2021-10-03 15:40:13 +0200

    feat: qlq test

--- js/script.js
+++ js/script.js
@@ -11,10 +11,9 @@ function ville(){
 
 // Fonction appelée lors du click du bouton
 function start() {
-  
   //récupérer ville à afficher
   city = ville();
-
+  
   // Création de l'objet apiWeather
   const apiWeather = new API_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
@@ -49,27 +48,27 @@ function getThreeDayForecast(){
   //récupérer ville à afficher
   city = ville();
 
-  // Création de l'objet apiWeather
-  const apiWeather = new API_3DAY_WEATHER(city);
+  // Création de l'objet api3DayWeather
+  const api3DayWeather = new API_3DAY_WEATHER(city);
   // Appel de la fonction fetchTodayForecast
 
-  apiWeather
+  api3DayWeather
     .fetchTodayForecast()
     .then(function(response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
       // On récupère l'information principal
-      const main = data.weather[0].main;
-      const description = data.weather[0].description;
+      const main = data.weather[1].main;
+      const description = data.weather[1].description;
       const temp = data.main.temp;
-      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+      const icon = api3DayWeather.getHTMLElementFromIcon(data.weather[1].icon);
 
       // Modifier le DOM
-      document.getElementById('today-forecast-main').innerHTML = main;
-      document.getElementById('today-forecast-more-info').innerHTML = description;
+      document.getElementById('tomorow-forecast-main').innerHTML = main;
+      document.getElementById('tomorow-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
-      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      document.getElementById('tomorow-forecast-temp').innerHTML = `${temp}°C`;
       
     })
     .catch(function(error) {
